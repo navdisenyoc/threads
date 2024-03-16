517bc0e49816c50563b0db12a435661dd7890ce7
navdisenyoc <157224324+navdisenyoc@users.noreply.github.com>
2024-03-16 11:12:54 +0530

    Add files via upload

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,8 +13,8 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads application",
+  title: "Naturron",
+  description: "A blockchain base social media AI app",
 };
 
 export default function RootLayout({
